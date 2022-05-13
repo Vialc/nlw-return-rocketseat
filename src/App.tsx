b63bf5ff8ee962818ff00b6f5f6ba106f3d84fc1
Vitor <costec222@gmail.com>
2022-05-13 02:02:16 -0300

    setando variavel de ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Dashboard } from "./pages/Dashboard";
 export function App() {
   return (
     <>
-      <Dashboard student_id="764eb50b-fec6-462b-b8c8-cd1be97a49b8" /*{import.meta.env.VITE_USER_TEST}*/ />
+      <Dashboard student_id= {import.meta.env.VITE_USER_TEST} />
       <Widget />
     </>
   );
