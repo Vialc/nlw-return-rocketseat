212d0f7479a93132c178bf49d7c828036914a108
Vitor <costec222@gmail.com>
2022-05-13 01:39:23 -0300

    corrigindo bug na variavel de ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Widget } from "./components/Widget";
 import { Dashboard } from "./pages/Dashboard";
 
-const studentId: string = import.meta.env.VITE_USER_TEST
+
 
 export function App() {
   return (
     <>
-      <Dashboard student_id={studentId} />
+      <Dashboard student_id={import.meta.env.VITE_USER_TEST} />
       <Widget />
     </>
   );
