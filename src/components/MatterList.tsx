3a6da0856ebd987b8797dea1b504899455beed6d
Vitor <costec222@gmail.com>
2022-05-15 21:20:28 -0300

    matter list bug fixe

--- src/components/MatterList.tsx
+++ src/components/MatterList.tsx
@@ -22,7 +22,7 @@ export function MatterList({ matters, MatterIdSelected }: MatterListProps) {
       <Listbox value={selected} onChange={e => handleMatterSelected(e)}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block text-black truncate">{selected.matter}</span>
+            <span className="block text-black dark:text-black truncate">{selected.matter}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <SortAscending
                 className="h-5 w-5 text-gray-800"
